274d741b05fe4a577dc5029232d68971966dd8af
Jonathan Rosenberg <jon.rosenberg@gmail.com>
2015-04-17 12:34:43 -0500

    refactored credentials

---
+++ app/services/parse-credentials.js
@@ -0,0 +1,6 @@
+pda = {};
+/* replace with your app keys */
+pda.parseCredentials = {
+	applicationID: 'APPLICATION-ID-HERE',
+	javascriptKey: 'JAVASCRIPT-KEY-HERE'	
+}
--- app/services/parse.js
+++ app/services/parse.js
@@ -3,5 +3,5 @@
 angular.module('parseServices', [])
 .factory('ParseSDK', function() {
   // pro-tip: swap these keys out for PROD keys automatically on deploy using grunt-replace
-  Parse.initialize("PdAqXwg7LfLBV5X0eR3H7sXeiLruJga2CVLEYTjf", "zRIqG95CEy8xrlOvBqSwFhAtoCWxaJP5HR6z9QWK");
+  Parse.initialize(pda.parseCredentials.applicationID, pda.parseCredentials.javascriptKey);
 });
